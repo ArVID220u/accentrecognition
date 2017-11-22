124a8a368469c0203996011718c9019d68159f58
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2017-11-22 15:13:24 +0100

    add splitfiles.py

---
+++ splitfiles.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+indirectory = input("in directory: ")
+outdirectory = input("out directory: ")
+import os
+
+segment_time = 5
+
+count = 1
+
+for filename in os.listdir(indirectory):
+    print("hej")
+    # split the file into 5 sec intervals
+    command = "ffmpeg -i " + indirectory + "/" + filename + " -f segment -segment_time " + str(segment_time) + " -c copy " + outdirectory + "/out" + "{:03}".format(count) + "%03d.mp3"
+    os.system(command)
+    print(command)
+    # remove the last clip since it probably is < 5 seconds
+    tempfiles = []
+    for tempfile in os.listdir(outdirectory):
+        if tempfile.startswith("out" + "{:03}".format(count)):
+            tempfiles.append(tempfile)
+    tempfiles.sort()
+    removefile = tempfiles[len(tempfiles)-1]
+    print(outdirectory + "/" + removefile)
+    count += 1
