12038b145e31355a2f5e21c896c9103bfe9613da
Teodor Bucht <teodor.bucht@gmail.com>
2017-12-03 21:33:00 +0100

    coordinator completed

--- code/coordinator.py
+++ code/coordinator.py
@@ -1,5 +1,77 @@
-import spectogram
+import spectogram as sp
 import numpy as np
+from pathlib import Path
+import random
+import network
 
-for i in range(0, 420):
+"""
+takes wav files and runs them through spectogram to convert them to trainging sets for nielsens code
+"""
+
+#begin with skanska
+
+training_data = []
+
+def buildpath(a, b, start):
+    a = str(a)
+    b = str(b)
+    start += (3 - len(a)) * "0"
+    start += (4 - len(b)) * "0"
+    start += ".wav"
+
+#This fucntion takes all files from the start_path-directory and adds them to training_data
+def create_data_set(start_path, correct_answer):
+
+    i = 1
+    j = 0
+
+    file_path = buildpath(i, j, start_path)
+    file = Path(file_path)
+
+    while(file.is_file()):
+    
+        specto = spectogram.spectogram(file_path)
+        specto = np.flatten(specto)
+        training_data.append((specto, correct_answer))
+        
+        file = Path(buildpath(i, j + 1, start_path))
+
+        if(file.is_file()):
+            j += 1
+            file_path = buildpath(i, j, start_path)
+        else:
+            j = 0
+            i += 1
+            file_path = buildpath(i, j, start_path)
+            file = Path(file_path)
+        
+
+create_data_set("../data/tmpvoices/skanska/fivesecfiles/", np.array([1, 0]))
+create_data_set("../data/tmpvoices/stockholmska/fivesecfiles/", np.array([0, 1]))
+
+#shuffle the training data to get random test_data
+random.shuffle(training_data)
+
+#splitting training_data into test_data and training_data
+test_data = training_data[(len(training_data) - 50):]
+training_data = training_data[:(len(training_data) - 50)]
+
+#data_points are the number of input neurons
+data_points = training_data[0][0].size
+
+#create the network object
+net = netork.Network([data_points, 10, 10, 2])
+
+#trains the network with the training data
+net.SGD(training_data, 1, 10, 3.0, test_data=test_data)
+
+#saving the weights and biases of the trained net
+weight_file = open('saved_weigths.txt', 'w')
+bias_file = open('saved_biases.txt', 'w')
+
+for item in net.weights:
+    print>>weight_file, item
+
+for item in net.biases:
+    print>>bias_file, item
 
