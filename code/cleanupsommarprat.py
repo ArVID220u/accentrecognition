da5e2736d23b5d1b468726d88cfef9fc57c7d398
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-01-06 15:35:01 +0100

    Make overlapping files for more data

--- code/cleanupsommarprat.py
+++ code/cleanupsommarprat.py
@@ -26,4 +26,28 @@ def shortenbeginning():
         command = "ffmpeg -y -i " + rfn + " -ss " + str(start_time) + " -acodec copy " + nfn
         os.system(command)
 
-shortenbeginning()
+def overlapfiles(fromdir):
+    # make files overlap, which creates a lot more data artificially. should improve our accuracy a lot
+    for filename in os.listdir(filedirectory):
+        rfn = filedirectory + "/"  + filename + "/" + fromdir + "/sommar.mp3"
+        if not os.path.exists(filedirectory + "/" + filename + "/" + "overlapfiles"):
+            os.makedirs(filedirectory + "/" + filename + "/" + "overlapfiles")
+        for i in range(0,6):
+            nfn = filedirectory + "/" + filename + "/" + "overlapfiles" + "/" + "sommar" + str(i) + ".mp3"
+            command = "ffmpeg -i " + rfn + " -ss " + str(i) + " -acodec copy " + nfn
+            os.system(command)
+
+def delfiles():
+    for filename in os.listdir(filedirectory):
+        os.system("rm -rf " + filedirectory + "/" + filename + "/" + "overlapfiles")
+
+def speedupfiles(fromdir):
+    # speed up some files to create a lot more artificial data. should improve accuracy perhaps
+    for filename in os.listdir(filedirectory):
+       dirn = filedirectory + "/" + filename + "/" + fromdir
+       counter = 0
+       for fn in os.lisdir(dirn):
+           pass
+
+
+overlapfiles("shortenedfiles")
