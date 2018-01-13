c2f4eeeb8538b610dea9608e5c44771ad596ad74
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-01-13 16:43:06 +0100

    convert network file from dos to unix

--- code/network.py
+++ code/network.py
@@ -1,6 +1,10 @@
+
+
 """
 network.py
 ~~~~~~~~~~
+
+
 
 A module to implement the stochastic gradient descent learning
 algorithm for a feedforward neural network.  Gradients are calculated
@@ -18,6 +22,48 @@ import json
 
 # Third-party libraries
 import numpy as np
+
+
+
+
+#### Define the quadratic and cross-entropy cost functions
+
+class QuadraticCost(object):
+
+    @staticmethod
+    def fn(a, y):
+        """Return the cost associated with an output a and desired output y."""
+        """ the norm of a the a-y column matrix is simply its absolute value if treated as a vector."""
+        return 0.5*np.linalg.norm(a-y)**2
+
+    @staticmethod
+    def delta(z, a, y):
+        """Return the error delta from the output layer."""
+        """This is the partial derivative of the cost function with respect
+        to each weighted input to each node in the last layer."""
+        return (a-y) * sigmoid_prime(z)
+
+class CrossEntropyCost(object):
+
+    @staticmethod
+    def fn(a, y):
+        """Return the cost associated with an output a and desired output y.
+        Note that np.nan_to_num is used to ensure numerical stability.
+        In particular, if both a and y have a 1.0 in the same slot,
+        then the expression (1-y)*np.log(1-a) returns nan.
+        The np.nan_to_num ensures that that is converted to the correct value (0.0)."""
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y):
+        """Return the error delta from the output layer.  Note that the
+        parameter z is not used by the method.  It is included in
+        the method's parameters in order to make the interface
+        consistent with the delta method for other cost classes."""
+        return (a-y)
+
+
+
 
 
 class Network(object):
@@ -286,38 +332,3 @@ def maxarg(x):
 
 
 
-#### Define the quadratic and cross-entropy cost functions
-
-class QuadraticCost(object):
-
-    @staticmethod
-    def fn(a, y):
-        """Return the cost associated with an output a and desired output y."""
-        """ the norm of a the a-y column matrix is simply its absolute value if treated as a vector."""
-        return 0.5*np.linalg.norm(a-y)**2
-
-    @staticmethod
-    def delta(z, a, y):
-        """Return the error delta from the output layer."""
-        """This is the partial derivative of the cost function with respect
-        to each weighted input to each node in the last layer."""
-        return (a-y) * sigmoid_prime(z)
-
-class CrossEntropyCost(object):
-
-    @staticmethod
-    def fn(a, y):
-        """Return the cost associated with an output a and desired output y.
-        Note that np.nan_to_num is used to ensure numerical stability.
-        In particular, if both a and y have a 1.0 in the same slot,
-        then the expression (1-y)*np.log(1-a) returns nan.
-        The np.nan_to_num ensures that that is converted to the correct value (0.0)."""
-        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
-
-    @staticmethod
-    def delta(z, a, y):
-        """Return the error delta from the output layer.  Note that the
-        parameter z is not used by the method.  It is included in
-        the method's parameters in order to make the interface
-        consistent with the delta method for other cost classes."""
-        return (a-y)
