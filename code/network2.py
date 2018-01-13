0ec3e3d741aba1c634ba8918e0ed79a8c5c04980
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-01-13 15:38:56 +0100

    initialize weights better to avoid saturation

--- code/network2.py
+++ code/network2.py
@@ -27,16 +27,31 @@ class Network(object):
         was [2, 3, 1] then it would be a three-layer network, with the
         first layer containing 2 neurons, the second layer 3 neurons,
         and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+        network are initialized randomly using default_weight_initializer."""
         self.num_layers = len(sizes)
         self.sizes = sizes
+        self.default_weight_initializer()
+
+    def default_weight_initializer(self):
+        """Initialize each weight using a Gaussian distribution with mean 0
+        and standard deviation 1 over the square root of the number of
+        weights connecting to the same neuron.  Initialize the biases
+        using a Gaussian distribution with mean 0 and standard
+        deviation 1.
+
+        Note that the first layer is assumed to be an input layer, and
+        by convention we won't set any biases for those neurons, since
+        biases are only ever used in computing the outputs from later
+        layers."""
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x)
-                        for x, y in zip(sizes[:-1], sizes[1:])]
+        self.weights = [np.random.randn(y, x)/np.sqrt(x) for x, y in zip(sizes[:-1], sizes[1:])]
+    
+    def large_weight_initializer(self):
+        """Initialize each weight and bias using Guassian distribution with mean 0
+        and standard deviation 1. Will lead to saturated hidden neurons and therefore to slow learning.
+        Only use it for comparison purposes."""
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
