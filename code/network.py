73cfa9d2b717187669d584e21b0651ec8bfb3e23
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-01-13 16:44:05 +0100

    convert network file formats

--- code/network.py
+++ code/network.py
@@ -1,11 +1,6 @@
-
-
 """
 network.py
 ~~~~~~~~~~
-
-
-
 A module to implement the stochastic gradient descent learning
 algorithm for a feedforward neural network.  Gradients are calculated
 using backpropagation.  Note that I have focused on making the code
